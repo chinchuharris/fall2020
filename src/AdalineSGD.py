6f0bb5491509e9ac04a5a6b04e8bc11c9fe46980
Philip Bogden <pbogdude@gmail.com>
2020-09-02 19:02:35 -0400

    update

--- src/AdalineSGD.py
+++ src/AdalineSGD.py
@@ -1,29 +1,4 @@
 class AdalineSGD(object):
-    """ADAptive LInear NEuron classifier.
-
-    Parameters
-    ------------
-    eta : float
-      Learning rate (between 0.0 and 1.0)
-    n_iter : int
-      Passes over the training dataset.
-    shuffle : bool (default: True)
-      Shuffles training data every epoch if True to prevent cycles.
-    random_state : int
-      Random number generator seed for random weight
-      initialization.
-
-
-    Attributes
-    -----------
-    w_ : 1d-array
-      Weights after fitting.
-    cost_ : list
-      Sum-of-squares cost function value averaged over all
-      training examples in each epoch.
-
-        
-    """
     def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
         self.eta = eta
         self.n_iter = n_iter
@@ -32,21 +7,6 @@ class AdalineSGD(object):
         self.random_state = random_state
         
     def fit(self, X, y):
-        """ Fit training data.
-
-        Parameters
-        ----------
-        X : {array-like}, shape = [n_examples, n_features]
-          Training vectors, where n_examples is the number of examples and
-          n_features is the number of features.
-        y : array-like, shape = [n_examples]
-          Target values.
-
-        Returns
-        -------
-        self : object
-
-        """
         self._initialize_weights(X.shape[1])
         self.cost_ = []
         for i in range(self.n_iter):
@@ -60,7 +20,6 @@ class AdalineSGD(object):
         return self
 
     def partial_fit(self, X, y):
-        """Fit training data without reinitializing the weights"""
         if not self.w_initialized:
             self._initialize_weights(X.shape[1])
         if y.ravel().shape[0] > 1:
@@ -71,18 +30,15 @@ class AdalineSGD(object):
         return self
 
     def _shuffle(self, X, y):
-        """Shuffle training data"""
         r = self.rgen.permutation(len(y))
         return X[r], y[r]
     
     def _initialize_weights(self, m):
-        """Initialize weights to small random numbers"""
         self.rgen = np.random.RandomState(self.random_state)
         self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=1 + m)
         self.w_initialized = True
         
     def _update_weights(self, xi, target):
-        """Apply Adaline learning rule to update the weights"""
         output = self.activation(self.net_input(xi))
         error = (target - output)
         self.w_[1:] += self.eta * xi.dot(error)
@@ -91,13 +47,10 @@ class AdalineSGD(object):
         return cost
     
     def net_input(self, X):
-        """Calculate net input"""
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
     def activation(self, X):
-        """Compute linear activation"""
         return X
 
     def predict(self, X):
-        """Return class label after unit step"""
         return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
